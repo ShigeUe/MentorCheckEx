c249bf388c38b834237625acfc552715cc9b975d
ShigeUe <16932213+ShigeUe@users.noreply.github.com>
2023-03-13 17:31:08 +0900

    フォント調査機能の要素特定方法を修正

--- mentor_check_ex/js/popup.js
+++ mentor_check_ex/js/popup.js
@@ -15,32 +15,43 @@ document.getElementById('FONT_CHECK_BUTTON').addEventListener('click', async (ev
   catch (e) {
     console.error(e);
   }
-  const { root } = await chrome.debugger.sendCommand(debuggee, 'DOM.getDocument');
+  const { root } = await chrome.debugger.sendCommand(debuggee, 'DOM.getDocument', { depth: -1 });
   await chrome.debugger.sendCommand(debuggee, 'CSS.enable');
-  const elements = await chrome.debugger.sendCommand(debuggee, 'DOM.querySelectorAll', { nodeId: root.nodeId, selector: '*' });
+  
   const allFonts = {};
-  for (let nodeId of elements.nodeIds) {
-    const { fonts } = await chrome.debugger.sendCommand(debuggee, "CSS.getPlatformFontsForNode", { nodeId });
-    if (!fonts.length) {
-      continue;
+  const findNodesThatContainText = async (node) => {
+    if (!node.nodeValue && !node.childNodeCount) {
+      return;
     }
+    if (node.nodeValue && node.nodeName != '#comment') {
+      const { fonts } = await chrome.debugger.sendCommand(debuggee, "CSS.getPlatformFontsForNode", { nodeId: node.nodeId });
+      if (fonts.length) {
+        for (let font of fonts) {
+          if (!font.isCustomFont) {
+            await chrome.debugger.sendCommand(debuggee, 'DOM.setAttributeValue', {
+              nodeId: node.parentId,
+              name: 'style',
+              value: 'background-color: rgba(255,0,0,0.5)'
+            });
+            allFonts[font.familyName + ' - ローカルフォント'] = font.isCustomFont;
+          }
+          else {
+            allFonts[font.familyName + ' - Webフォント'] = font.isCustomFont;
+          }
+        }
+      }
+    }
+    if (node.childNodeCount) {
+      for (let i = 0; i < node.childNodeCount; i++) {
+        await findNodesThatContainText(node.children[i]);
+      }
+    }
+  };
 
-    for (let font of fonts) {
-      if (!font.isCustomFont) {
-        await chrome.debugger.sendCommand(debuggee, 'DOM.setAttributeValue', {
-          nodeId,
-          name: 'style',
-          value: 'background-color: rgba(255,0,0,0.7)'
-        });
-        allFonts[font.familyName + ' - ローカルフォント'] = font.isCustomFont;
-      }
-      else {
-        allFonts[font.familyName + ' - Webフォント'] = font.isCustomFont;
-      }
-    }
+  for (let node of root.children) {
+    await findNodesThatContainText(node);
   }
-  console.log(allFonts);
-
+  
   try {
     await chrome.debugger.detach(debuggee);
   }
