aff279548b77c0813089398de6695956c010e7c5
ShigeUe <16932213+ShigeUe@users.noreply.github.com>
2025-04-12 01:43:19 +0900

    Googleドライブが複数あるときに正しいものを選択

--- src/js/open_gdrive.ts
+++ src/js/open_gdrive.ts
@@ -3,8 +3,29 @@ import { Util } from "./util";
 import { CurriculumIdToData } from "./curriculum_codes";
 
 (async () => {
-  const link = Util.query<HTMLAnchorElement>('[href^="https://drive.google.com/drive/"]:has(i)', true);
-  if (link === null) {
+  // 課題のリンクが存在すれば課題レビューページ、存在しなければユーザーページ
+  const kadai_link = Util.query<HTMLAnchorElement>(`#page-content-wrapper .col-sm-8 table td a[href*="#kadai-"]`, true);
+  let link: HTMLAnchorElement | null = null;
+    
+  if (kadai_link) {
+    // 課題レビューはGoogleドライブのリンクはひとつ
+    link = Util.query<HTMLAnchorElement>('[href^="https://drive.google.com/drive/"]:has(i)', true);
+  }
+  else {
+    // 複数のGoogleドライブのリンクがある可能性があるので絞り込む
+    const links = Util.queryAll<HTMLAnchorElement>('[href^="https://drive.google.com/drive/"]:has(i)');
+    if (links.length === 0) {
+      return;
+    }
+  
+    links.forEach((el) => {
+      if (el.innerText.includes('はじめての副業')) {
+        link = el;
+      }
+    });
+  }
+  if (!link) {
+    // Googleドライブへのリンクが取得できなければ終了
     return;
   }
 
@@ -15,8 +36,31 @@ import { CurriculumIdToData } from "./curriculum_codes";
   // GoogleフォルダID
   const f_id = (new URL(link.href)).pathname.split('/').pop();
 
-  const kadai_link = Util.query<HTMLAnchorElement>(`#page-content-wrapper .col-sm-8 table td a[href*="#kadai-"]`, true);
-  if (!kadai_link) { // 課題のリンクがない＝ユーザーページ
+  if (kadai_link) {
+    // 課題レビュー
+    const curriculum_id = kadai_link.href.split('#').pop() as string;
+    if (!CurriculumIdToData[curriculum_id]) {
+      return;
+    }
+    const target = CurriculumIdToData[curriculum_id].folder;
+    if (!target) {
+      return;
+    }
+
+    const div = document.createElement('div');
+    const small = document.createElement('p');
+
+    if (settings.diff) {
+      const user = Util.query<HTMLAnchorElement>('[href^="/mentor/users/').href.split('/').pop();
+      small.classList.add('add-margin-0');
+      small.innerHTML = small.innerHTML +
+        (CurriculumIdToData[curriculum_id] ? `<a href="/mentor/courses/first-sidejob/curriculums/first-sidejob-2/lessons?diff=1&id=${f_id}&folder=${target}&c_id=${curriculum_id}&user=${user}" target="_blank" class="btn btn-success btn-sm font-size-x-small">レビュー比較ツール</a><br><small>（初回BASIC認証が求められます）</small>\n` : '');
+      div.append(small);
+    }
+    link.parentElement && link.parentElement.append(div);
+  }
+  else {
+    // ユーザーページ
     if (settings.diff) {
       const user = location.href.split('/').pop();
 
@@ -33,30 +77,5 @@ import { CurriculumIdToData } from "./curriculum_codes";
       });
       link.parentElement && link.parentElement.append(div);
     }
-    return;
-  }
-  const curriculum_id = kadai_link.href.split('#').pop() as string;
-  if (!CurriculumIdToData[curriculum_id]) {
-    return;
-  }
-  const target = CurriculumIdToData[curriculum_id].folder;
-  if (!target) {
-    return;
-  }
-
-
-
-
-  const div = document.createElement('div');
-  const small = document.createElement('p');
-
-  if (settings.diff) {
-    const user = Util.query<HTMLAnchorElement>('[href^="/mentor/users/').href.split('/').pop();
-    small.classList.add('add-margin-0');
-    small.innerHTML = small.innerHTML +
-      (CurriculumIdToData[curriculum_id] ? `<a href="/mentor/courses/first-sidejob/curriculums/first-sidejob-2/lessons?diff=1&id=${f_id}&folder=${target}&c_id=${curriculum_id}&user=${user}" target="_blank" class="btn btn-success btn-sm font-size-x-small">レビュー比較ツール</a><br><small>（初回BASIC認証が求められます）</small>\n` : '');
-    div.append(small);
-  }
-  link.parentElement && link.parentElement.append(div);
-  
+  }  
 })();
